f8c6660efd5fd9cd4976a4984e44994bb997225a
Stoilov <stilian.stoilov@sap.com>
2022-12-19 10:30:54 +0200

    Little improvements in etcd_bootstrap_script.sh

--- etcd_bootstrap_script.sh
+++ etcd_bootstrap_script.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
+# Shellchecked: All good
 
 VALIDATION_MARKER=/var/etcd/data/validation_marker
 
 # Add self-signed CA to list of root CA-certificates
-if [ $ENABLE_TLS = 'true' ]
-then
-  cat /var/etcd/ssl/client/ca/*.crt >> /etc/ssl/certs/ca-certificates.crt
-  if [ $? -ne 0 ]
-  then
-    echo "failed to update root certificate list"
-    exit 1
-  fi
+if [ "${ENABLE_TLS}" = 'true' ]; then
+    if ! cat /var/etcd/ssl/client/ca/*.crt >> /etc/ssl/certs/ca-certificates.crt; then
+        echo "failed to update root certificate list"
+        exit 1
+    fi
 fi
 
+# shellcheck disable=SC2064
 trap_and_propagate() {
     PID=$1
     shift
@@ -22,70 +21,66 @@ trap_and_propagate() {
 }
 
 start_managed_etcd(){
-      arch=$(uname -m)
-      if [ $arch = "aarch64" ] || [ $arch = "arm64" ]; then
-          # Running etcd on ARM has experimental support for version 3.4.x
-          # https://etcd.io/docs/v3.4/op-guide/supported-platform/
-          export ETCD_UNSUPPORTED_ARCH=arm64
-      fi
-      rm -rf $VALIDATION_MARKER
-      CONFIG_FILE=/etc/etcd.conf.yaml
-      curl --retry 5 --retry-delay 5 --retry-all-errors "$BACKUP_ENDPOINT/config" -o $CONFIG_FILE
-      minimumsize=50
-      actualsize=$(wc -c <$CONFIG_FILE)
-      if [ $actualsize -le $minimumsize ]; then
-          echo "downloaded config file size is less than $(minimumsize) bytes"
-          exit 1
-      fi
-      etcd --config-file $CONFIG_FILE &
-      ETCDPID=$!
-      trap_and_propagate $ETCDPID INT TERM
-      wait $ETCDPID
-      RET=$?
-      echo $RET > $VALIDATION_MARKER
-      exit $RET
+    arch=$(uname -m)
+    if [ $arch = "aarch64" ] || [ $arch = "arm64" ]; then
+        # Running etcd on ARM has experimental support for version 3.4.x
+        # https://etcd.io/docs/v3.4/op-guide/supported-platform/
+        export ETCD_UNSUPPORTED_ARCH=arm64
+    fi
+    rm -rf $VALIDATION_MARKER
+    CONFIG_FILE=/etc/etcd.conf.yaml
+    curl --retry 5 --retry-delay 5 --retry-all-errors "$BACKUP_ENDPOINT/config" -o $CONFIG_FILE
+    minimumsize=50
+    actualsize=$(wc -c <$CONFIG_FILE)
+    if [ "${actualsize}" -le "${minimumsize}" ]; then
+        echo "downloaded config file size is less than $(minimumsize) bytes"
+        exit 1
+    fi
+    etcd --config-file $CONFIG_FILE &
+    ETCDPID=$!
+    trap_and_propagate $ETCDPID INT TERM
+    wait $ETCDPID
+    RET=$?
+    echo $RET > $VALIDATION_MARKER
+    exit $RET
 }
 
 check_and_start_etcd(){
-      while true;
-      do
-        wget "$BACKUP_ENDPOINT/initialization/status" -S -O status;
-        STATUS=$(cat status);
+    while true; do
+        wget "$BACKUP_ENDPOINT/initialization/status" -S -O status
+        STATUS=$(cat status)
         case $STATUS in
-        "New")
-              wget "$BACKUP_ENDPOINT/initialization/start?mode=$1$FAIL_BELOW_REVISION_PARAMETER" -S -O - ;;
-        "Progress")
-              sleep 1;
-              continue;;
-        "Failed")
-              sleep 1;
-              continue;;
-        "Successful")
-              echo "Bootstrap preprocessing end time: $(date)"
-              start_managed_etcd
-              break
-              ;;
-        *)
-              sleep 1;
-              ;;
-        esac;
-      done
+            "New")
+                wget "$BACKUP_ENDPOINT/initialization/start?mode=$1$FAIL_BELOW_REVISION_PARAMETER" -S -O - ;;
+            "Progress")
+                sleep 1
+                continue ;;
+            "Failed")
+                sleep 1
+                continue ;;
+            "Successful")
+                echo "Bootstrap preprocessing end time: $(date)"
+                start_managed_etcd
+                break ;;
+            *)
+                sleep 1 ;;
+        esac
+    done
 }
 
 echo "Bootstrap preprocessing start time: $(date)"
-if [ ! -f $VALIDATION_MARKER ] ;
-then
-      echo "No $VALIDATION_MARKER file. Perform complete initialization routine and start etcd."
-      check_and_start_etcd full
+if [ ! -f $VALIDATION_MARKER ]; then
+    echo "No $VALIDATION_MARKER file. Perform complete initialization routine and start etcd."
+    check_and_start_etcd full
 else
-      echo "$VALIDATION_MARKER file present. Check return status and decide on initialization"
-      run_status=$(cat $VALIDATION_MARKER)
-      echo "$VALIDATION_MARKER content: $run_status"
-      if [ $run_status = '143' ] || [ $run_status = '130' ] || [ $run_status = '0' ] ; then
-            echo "Requesting sidecar to perform sanity validation"
-            check_and_start_etcd sanity
-      else
-            echo "Requesting sidecar to perform full validation"
-            check_and_start_etcd full
-      fi
+    echo "$VALIDATION_MARKER file present. Check return status and decide on initialization"
+    run_status=$(cat $VALIDATION_MARKER)
+    echo "$VALIDATION_MARKER content: $run_status"
+    if [ "${run_status}" = '143' ] || [ "${run_status}" = '130' ] || [ "${run_status}" = '0' ]; then
+        echo "Requesting sidecar to perform sanity validation"
+        check_and_start_etcd sanity
+    else
+        echo "Requesting sidecar to perform full validation"
+        check_and_start_etcd full
+    fi
 fi
