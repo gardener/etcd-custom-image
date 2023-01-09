f7c2b1809471056090784727e825a6147dabc50a
Aaron Francis Fernandes <79958509+aaronfern@users.noreply.github.com>
2023-01-09 17:44:30 +0530

    Retry curl for etcd configuration in case of error (#29)

--- etcd_bootstrap_script.sh
+++ etcd_bootstrap_script.sh
@@ -24,7 +24,7 @@ trap_and_propagate() {
 start_managed_etcd(){
       rm -rf $VALIDATION_MARKER
       CONFIG_FILE=/etc/etcd.conf.yaml
-      curl "$BACKUP_ENDPOINT/config" -o $CONFIG_FILE
+      curl --retry 5 --retry-delay 5 --retry-all-errors "$BACKUP_ENDPOINT/config" -o $CONFIG_FILE
       minimumsize=50
       actualsize=$(wc -c <$CONFIG_FILE)
       if [ $actualsize -le $minimumsize ]; then
